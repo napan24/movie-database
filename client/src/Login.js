c2e50f1e41ad304795afec147a1679f349947748
napan24 <napanv24@gmail.com>
2022-06-24 17:11:35 +0530

    improved login

--- client/src/Login.js
+++ client/src/Login.js
@@ -37,26 +37,26 @@ export const Login = () => {
         <>
             <div className="body" style={{ width: "100vw", height: "100vh", backgroundImage: `url(${background})`, backgroundSize: "cover", backgroundRepeat: "no-repeat" }}>
                 <form method='POST'>
-                    <div className="box" style={{ width: "30vw", height: "80vh", backgroundColor: "white", borderRadius: "10px", position: "absolute", left: "0", right: "0", top: "0", bottom: "0", margin: "auto", fontFamily: "Arial, sans-serif" }}>
-                        <h3 style={{ fontSize: "3vw", position: "absolute", left: "30%", top: "0%" }}>
+                    <div className="box" style={{  backgroundColor: "white", width:"70vmin",height:"80vmin", borderRadius: "10px", position: "absolute", left: "0", right: "0", top: "0", bottom: "0", margin: "auto", fontFamily: "Arial, sans-serif" }}>
+                        <h3 style={{ fontSize: "7vmin", position: "absolute", left: "30%", top: "0%" }}>
                             Sign In
                         </h3>
-                        <div style={{ position: "absolute", top: "25%", left: "8%" }}>
+                        <div style={{ position: "absolute", top: "25%", left: "8%",fontSize:"3vmin" }}>
                             Email
                         </div>
-                        <input type="text" className="fontAwesome" value={data.email} placeholder="Type Your Email" name="email" style={{ position: "absolute", top: "32%", left: "8%",width:"80%",height:"5%",border:"none",borderBottom:"2px solid black",outline:"none" }} onChange={inputChange}/>
-                        <div style={{ position: "absolute", top: "45%", left: "8%" }}>
+                        <input type="text" className="fontAwesome" value={data.email} placeholder="Type Your Email" name="email" style={{ position: "absolute", top: "32%", left: "8%",width:"80%",height:"5%",border:"none",borderBottom:"2px solid black",outline:"none",fontSize:"3vmin" }} onChange={inputChange}/>
+                        <div style={{ position: "absolute", top: "45%", left: "8%",fontSize:"3vmin"}}>
                             Password
                         </div>
-                        <input type="password" placeholder="Type Your Password" value={data.password} name="password" style={{ position: "absolute", top: "52%", left: "8%",width:"80%",height:"5%",border:"none",borderBottom:"2px solid black",outline:"none" }} onChange={inputChange}/>
-                        <button onClick={handleSubmit} style={{ position: "absolute", top: "65%", left: "8%" ,width:"80%",height:"7%",border:"none",backgroundColor:"black",color:"white",borderRadius:"10px"}}>Sign In</button>
-                        <div style={{position: "absolute", top: "75%", left: "30%",fontSize:"1.5vw"}}>
+                        <input type="password" placeholder="Type Your Password" value={data.password} name="password" style={{ position: "absolute", top: "52%", left: "8%",width:"80%",height:"5%",border:"none",borderBottom:"2px solid black",outline:"none",fontSize:"3vmin" }} onChange={inputChange}/>
+                        <button onClick={handleSubmit} style={{ fontSize:"3vmin",position: "absolute", top: "65%", left: "8%" ,width:"80%",height:"7%",border:"none",backgroundColor:"black",color:"white",borderRadius:"10px"}}>Sign In</button>
+                        <div style={{position: "absolute", top: "75%", left: "30%",fontSize:"3vmin"}}>
                             <bold>Or Sign Up Using</bold>
                         </div>
                         <img src={google_logo} style={{position: "absolute", top: "82%", left: "15%",width:"10%"}}></img>
                         <img src={facebook_logo} style={{position: "absolute", top: "81%", left: "70%",width:"13%"}}></img>
                         <Link to="signup">
-                            <button style={{ position: "absolute", top: "93%", left: "8%" ,width:"85%",height:"6%",border:"none",backgroundColor:"black",color:"white",borderRadius:"10px"}}>Sign Up</button>
+                            <button style={{ fontSize:"3vmin",position: "absolute", top: "93%", left: "8%" ,width:"85%",height:"6%",border:"none",backgroundColor:"black",color:"white",borderRadius:"10px"}}>Sign Up</button>
                         </Link>
                     </div>
                 </form>
